c8f7297cfdf9b894dc1215c6ae6a9aea61aa43dd
vik denic <vik.denic@gmail.com>
2015-08-30 09:08:50 -0500

    Implement onClickListeners to continue flow of story

--- app/src/main/java/com/nektarlabs/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/nektarlabs/interactivestory/ui/StoryActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,6 +24,7 @@ public class StoryActivity extends Activity {
     private Button mChoice1;
     private Button mChoice2;
     private String mName;
+    private Page mCurrentPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +44,37 @@ public class StoryActivity extends Activity {
         mChoice1 = (Button) findViewById(R.id.choiceButton1);
         mChoice2 = (Button) findViewById(R.id.choiceButton2);
 
-        loadPage();
+        loadPage(0);
     }
 
-    private void loadPage() {
-        Page page = mStory.getPage(0);
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
 
-        Drawable drawable = getResources().getDrawable(page.getImageId());
+        Drawable drawable = getResources().getDrawable(mCurrentPage.getImageId());
         mImageView.setImageDrawable(drawable);
 
-        String pageText = page.getText();
+        String pageText = mCurrentPage.getText();
         // Add the name if placeholder included. Won't add if no placeholder
         pageText = String.format(pageText, mName);
         mTextView.setText(pageText);
 
-        mChoice1.setText(page.getChoice1().getText());
-        mChoice2.setText(page.getChoice2().getText());
+        mChoice1.setText(mCurrentPage.getChoice1().getText());
+        mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+        mChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = mCurrentPage.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        mChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = mCurrentPage.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
     }
 }
