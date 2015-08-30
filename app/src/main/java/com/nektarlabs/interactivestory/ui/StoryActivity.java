51116cf7b94a395bd8dfdf8a9b0470ca2e8ddb5f
vik denic <vik.denic@gmail.com>
2015-08-30 08:52:36 -0500

    Load Story page with user's name
    Loads page0 and passes name into placeholder

--- app/src/main/java/com/nektarlabs/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/nektarlabs/interactivestory/ui/StoryActivity.java
@@ -2,17 +2,27 @@ package com.nektarlabs.interactivestory.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nektarlabs.interactivestory.R;
 import com.nektarlabs.interactivestory.model.Page;
+import com.nektarlabs.interactivestory.model.Story;
 
 public class StoryActivity extends Activity {
 
-    private Page[] mPages;
-
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +30,33 @@ public class StoryActivity extends Activity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "Friend";
+        if (mName == null) {
+            mName = "Friend";
         }
+        Log.d(TAG, mName);
 
-        Log.d(TAG, name);
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
 
-        mPages = new Page[7];
+        loadPage();
+    }
+
+    private void loadPage() {
+        Page page = mStory.getPage(0);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = page.getText();
+        // Add the name if placeholder included. Won't add if no placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
     }
 }
